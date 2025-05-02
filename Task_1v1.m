08d3747ae649926f3d7449a0638c7cd3baae053b
MarkLeipi <markleipi@web.de>
2025-05-03 00:47:13 +0200

    Lakschuh checke nicht wie Maske implementiert werden soll

--- Task_1v1.m
+++ Task_1v1.m
@@ -1,22 +1,30 @@
 % Zugriff auf die Nifti-Dateien
 
 CaseID = 66; % CaseID manuell angeben
+
+allCases = "allcasesunzipped"; % Ordner, der die CaseFiles enthält
+CaseFile = sprintf("case_%05d", CaseID);  % Pfad des Case-Ordners (fünf Stellen mit führenden Nullen vor CaseNumber")
+PathImag = fullfile(allCases, CaseFile, "imaging.nii.gz");  % Pfad zur imaging-Datei
+PathMask = fullfile(allCases, CaseFile, "segmentation.nii.gz");  % Pfad zur segmanting-Datei
+NiiImage = niftiread(PathImag);  % Einlesen der Imaging-Datei
+NiiIMask = niftiread(PathMask);  % Einlesen der segmanting-Datei
+
+% Vorbereitung der Daten
 table = readtable("patients_25.xlsx"); % Laden der Excel-Datei
 RowIndex = CaseID == table{:,1}; % gibt Zeilenindex des passenden Cases wider
-allCases = "allcasesunzipped"; % Ordner, der die CaseFiles enthält
-CaseFile = sprintf("case_%05d", CaseID);  % Name des Case-Ordners (fünf Stellen mit führenden Nullen vor CaseNumber")
-PathImag = fullfile(allCases, CaseFile, "imaging.nii.gz");  % Pfad zur imaging-Datei
-PathMask = fullfile(allCases, CaseFile, "segmentation.nii.gz");  % Pfad zur imaging-Datei
-NiiImage = niftiread(PathImag);  % Einlesen der Imaging-Datei
-NiiIMask = niftiread(PathMask);  % Einlesen der Maske
 
-info = niftiinfo(imaging.nii.gz)
+% Speichern der Daten die nur über Excel-Tabelle verfügbar sind
+Xslice = table{RowIndex, 9};  % Spalte I = 9
+Zslice_min = table{RowIndex, 10};  % Spalte J = 10
+Zslice_max = table{RowIndex, 11};  % Spalte K = 11
+% wo wird der Y-Schnitt gesetzt?
+InfoImag = niftiinfo(PathImag); % Eigenschaften der Nifti-Datei
+VoxelSize = InfoImag.PixelDimensions; % Voxel Dimensionen als 1x3 Vektor
 
 Max = max(NiiImage,[],"all"); % Maximum der gesamten Datei 
 Min = min(NiiImage,[],"all"); % Minimum der gesamten Datei
-
 Image_normal = (NiiImage - Min)./(Max - Min); % elementweise Normalisierung der Werte
-dim = size(Va_normal); % Dimension des Volumens
+dim = size(Image_normal); % Dimension des Volumens
 
 % 2D - Visualisierung 
 
@@ -27,9 +35,9 @@ midSliceX = round(dimX/2); % Mittelschnitt X
 midSliceY = round(dimY/2); % Mittelschnitt Y
 midSliceZ = round(dimZ/2); % Mittelschnitt Z
 
-sagittal = Va_normal(:,:,midSliceZ); % sagittal "links und rechts" , fixes Z
-koronal = Va_normal(:,midSliceY,:);% "koronal "vorne und hinten", fixes Y
-axial = Va_normal(midSliceX,:,:); % axial "oben und unten", fixes X
+sagittal = Image_normal(:,:,midSliceZ); % sagittal "links und rechts" , fixes Z
+koronal = Image_normal(:,midSliceY,:);% "koronal "vorne und hinten", fixes Y
+axial = Image_normal(midSliceX,:,:); % axial "oben und unten", fixes X
 
 unten2D = squeeze(axial); 
 seitlich2D = squeeze(sagittal);
@@ -39,7 +47,7 @@ frontal2D = squeeze(koronal); % Entfernen der überflüssigen Dimension um zweid
 
 subplot(1,3,1);
 imshow(frontal2D);
-title('Koronal');
+title('Koronar');
 
 subplot(1,3,2);
 imshow(unten2D);
@@ -49,5 +57,5 @@ subplot(1,3,3);
 imshow(seitlich2D);
 title('Sagittal');
 
-% 3D- Visualisierunge
+% 3D- Visualisierung
 %volshow (Image_normal);
