5004634ff150ab739130af878e435e3bae6707cf
Sven Schmitt <svenschmitt01@gmail.com>
2025-05-12 14:30:26 +0200

    testen der parameter

--- Kantendetektion.m
+++ Kantendetektion.m
@@ -1,43 +1,19 @@
-%% Vorverarbeitungsschritte
-
-% Original-Slice
-I_orig = slice_cor;
-
-% Gauß-Glättung
-I_gauss = imgaussfilt(I_orig, 1.0);
-
-% Medianfilter
-I_med = medfilt2(I_orig, [3 3]);
-
-% Bilateralfilter
-I_bilat = imbilatfilt(I_orig);
-
-% Anisotrope Diffusion
-I_diff = imdiffusefilt(I_orig, 'NumberOfIterations', 15, 'GradientThreshold', 10);
-
-%% Klassische Kantenerkennung (Canny)
-BW_orig = edge(I_orig, 'Canny');
-BW_gauss = edge(I_gauss, 'Canny');
-BW_med = edge(I_med, 'Canny');
-BW_bilat = edge(I_bilat, 'Canny');
-BW_diff = edge(I_diff, 'Canny');
-
 %% Fuzzy-basierte Kantenerkennung (über Gradientenmagnitude)
 
 % Gradient berechnen (auf geglättetem Bild, z.B. Median)
-[Gmag, ~] = imgradient(I_med);
+[Gmag, ~] = imgradient(I_bilat);
 
 % Erzeuge Mamdani-FIS
 fis = mamfis('Name','EdgeFuzzy');
 fis = addInput(fis, [0 1], 'Name', 'Gradient');
-fis = addMF(fis, 'Gradient', 'gaussmf', [0.1 0], 'Name', 'low');
-fis = addMF(fis, 'Gradient', 'gaussmf', [0.1 0.5], 'Name', 'medium');
-fis = addMF(fis, 'Gradient', 'gaussmf', [0.1 1], 'Name', 'high');
+fis = addMF(fis, 'Gradient', 'gaussmf', [0.05 0], 'Name', 'low');
+fis = addMF(fis, 'Gradient', 'gaussmf', [0.05 0.3], 'Name', 'medium');
+fis = addMF(fis, 'Gradient', 'gaussmf', [0.05 0.7], 'Name', 'high');
 
 fis = addOutput(fis, [0 1], 'Name', 'EdgeStrength');
-fis = addMF(fis, 'EdgeStrength', 'trimf', [0 0 0.5], 'Name', 'weak');
-fis = addMF(fis, 'EdgeStrength', 'trimf', [0 0.5 1], 'Name', 'medium');
-fis = addMF(fis, 'EdgeStrength', 'trimf', [0.5 1 1], 'Name', 'strong');
+fis = addMF(fis, 'EdgeStrength', 'trimf', [0 0 0.3], 'Name', 'weak');
+fis = addMF(fis, 'EdgeStrength', 'trimf', [0.2 0.4 0.8], 'Name', 'medium');
+fis = addMF(fis, 'EdgeStrength', 'trimf', [0.7 1 1], 'Name', 'strong');
 
 rules = [
     "If Gradient is low Then EdgeStrength is weak"
@@ -53,10 +29,17 @@ for i = 1:size(Gmag,1)
         edge_fuzzy(i,j) = evalfis(fis, Gmag(i,j));
     end
 end
-%% Vergleich der Vorverarbeitungsschritte 
+
+%% Vergleich der Vorverarbeitungsschritte  mit Canny
 
 % Schritt 1: Glättung (linear)
-I_gauss = imgaussfilt(slice_cor, 1.0);  % Sigma = 1.0
+G = [1 2 1; 
+     2 4 2; 
+     1 2 1];
+G = G / sum(G(:));  % Normieren auf Summe = 1
+
+% Faltung mit dem Bild
+I_gauss_manual = conv2(double(I_orig), G, 'same');
 
 % Schritt 2: Glättung (nichtlinear)
 I_med = medfilt2(slice_cor, [3 3]);  % Medianfilter --> besser für Kantenerhalt
@@ -68,16 +51,21 @@ I_bilat = imbilatfilt(slice_cor);  % Erhält Kanten besser
 I_diff = imdiffusefilt(slice_cor, 'NumberOfIterations', 15, 'GradientThreshold', 10);
 
 % Schritt 5: Kantenerkennung danach (z. B. Canny)
-BW_orig = edge(slice_cor, 'Canny');
-BW_gauss = edge(I_gauss, 'Canny');
+BW_orig = edge(slice_cor, 'sobel');
+BW_gauss = edge(I_gauss_manual, 'prewitt');
 BW_med = edge(I_med, 'Canny');
-BW_bilat = edge(I_bilat, 'Canny');
+BW_bilat = edge(I_bilat, 'sobel');
 BW_diff = edge(I_diff, 'Canny');
+% Binär machen mit adaptivem Schwellwert
+BW_fuzzy = imbinarize(edge_fuzzy, graythresh(edge_fuzzy));
+threshold = graythresh(edge_fuzzy);  % Berechnet den besten Schwellenwert
+disp(threshold);
 
 % Vergleich
 figure;
-subplot(2,3,1); imshow(BW_orig); title('Original + Canny');
-subplot(2,3,2); imshow(BW_gauss); title('Gauß + Canny');
+subplot(2,3,1); imshow(BW_orig); title('Original + sobel');
+subplot(2,3,2); imshow(BW_gauss); title('Gauß + prewitt');
 subplot(2,3,3); imshow(BW_med); title('Median + Canny');
-subplot(2,3,4); imshow(BW_bilat); title('Bilateral + Canny');
-subplot(2,3,5); imshow(BW_diff); title('Diffusion + Canny');  
+subplot(2,3,4); imshow(BW_bilat); title('Bilateral + sobel');
+subplot(2,3,5); imshow(BW_diff); title('Diffusion + Canny'); 
+subplot(2,3,6); imshow(BW_fuzzy); title('Bilateral + Fuzzy');
